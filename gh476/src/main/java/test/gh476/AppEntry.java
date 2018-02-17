cf268907aa31c0446c2f79b27d850d1af2fc8107
Gelin Luo <greenlaw110@gmail.com>
2018-02-17 19:03:35 +1100

    add gh476 verification project

---
+++ gh476/src/main/java/test/gh476/AppEntry.java
@@ -0,0 +1,12 @@
+package test.gh476;
+
+import act.Act;
+
+@SuppressWarnings("unused")
+public class AppEntry {
+
+    public static void main(String[] args) throws Exception {
+        Act.start();
+    }
+
+}
